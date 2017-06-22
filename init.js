74516dc6bdbe8910162e783994a915884b70e7e7
Luan Andryl <luan.andryl@gmail.com>
2017-06-22 17:40:11 -0300

    lazy commit

---
+++ init.js
@@ -0,0 +1,53 @@
+var express = require('express'); 
+var fs = require('fs');
+var cors = require('cors');
+
+var app = express();
+app.use(cors());
+
+var state = false;
+
+var arduinoRequest = new Date();
+
+function appendObject(obj) {
+
+  var config = JSON.parse(fs.readFileSync('./data.txt'));
+  config['logs'].push(obj);
+  var configJSON = JSON.stringify(config);
+  fs.writeFileSync('./data.txt', configJSON);
+
+}
+
+app.get('/', function(req, res) {
+  res.send(state);
+});
+
+app.post('/', function (req, res) {
+  res.send(state = !state);
+  appendObject({
+    "date" : new Date(),
+    "state": state
+  });
+});
+
+app.get('/dispositivo/', function(req, res) {
+  if((new Date() - arduinoRequest) > 600)
+    res.status(500).send('Device Not Conected');
+  else
+    res.status(200).send('OK');
+});
+
+app.get('/data/', function (req, res) {
+  res.send(JSON.parse(fs.readFileSync('./data.txt')));
+});
+
+app.get('/:quem', function (req, res) {
+  if (req.params.quem)
+    arduinoRequest = new Date();
+  
+  res.send(state);
+});
+
+app.listen(3000, function () {
+  console.log('web app listening on port 3000!');
+});
